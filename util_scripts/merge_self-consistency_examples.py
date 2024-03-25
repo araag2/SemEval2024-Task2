c19d47fb11bd296818551e245461079b1926d85d
araag2 <artur.guimas@gmail.com>
2024-03-25 17:30:48 +0000

    removed extra </s>

--- util_scripts/merge_self-consistency_examples.py
+++ util_scripts/merge_self-consistency_examples.py
@@ -11,6 +11,7 @@ def safe_open_w(path: str):
 def main():
     parser = argparse.ArgumentParser()
 
+    parser.add_argument('--exp_name', type=str, help='name of the experiment', default='explain_e-c')
     parser.add_argument('--og_queries', type=str, default='../queries/queries2024_test.json')
     parser.add_argument('--entail_queries', type=str, default='../outputs/entailment2024-03-25_15-40_dev-set.json')
     parser.add_argument('--contradict_queries', type=str, default='../outputs/contradiction2024-03-25_15-38_dev-set.json')
@@ -24,10 +25,11 @@ def main():
 
     for q_id in tqdm(og_q):
         res[f'{q_id}'] = og_q[q_id]
-        res[f'{q_id}']["entail"] = entail_q[q_id]["expanded_text"] 
-        res[f'{q_id}']["contradict"] = contradict_q[q_id]["expanded_text"]
+        res[f'{q_id}']["explain"] = entail_q[q_id]["expanded_text"][:-4] 
+        #res[f'{q_id}']["entail"] = entail_q[q_id]["expanded_text"] 
+        #res[f'{q_id}']["contradict"] = contradict_q[q_id]["expanded_text"]
 
-    with safe_open_w(f'{args.og_queries[:-5]}_self-consistency.json') as out_f:
+    with safe_open_w(f'{args.og_queries[:-5]}_{args.exp_name}.json') as out_f:
         json.dump(res, out_f, indent=4)
 
 if __name__ == '__main__':
