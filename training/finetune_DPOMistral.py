675df9eb6f0601babf3c2eabd7a045677cca61e5
araag2 <artur.guimas@gmail.com>
2024-07-12 02:50:04 +0100

    updated DPO Training

--- training/finetune_DPOMistral.py
+++ training/finetune_DPOMistral.py
@@ -11,7 +11,7 @@ from datasets.arrow_dataset import Dataset
 # Model Libs
 from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig, AutoTokenizer, TrainingArguments
 from peft import LoraConfig, prepare_model_for_kbit_training, get_peft_model, PeftModel
-from trl import DPOTrainer, DPODataCollatorWithPadding
+from trl import DPOTrainer, DataCollatorForCompletionOnlyLM
 
 def create_path(path : str) -> None:
     os.makedirs(path, exist_ok=True)
@@ -24,31 +24,31 @@ def parse_args():
     parser.add_argument('--model_name', type=str, default="mistralai/Mistral-7B-Instruct-v0.2", help='model to train')
     parser.add_argument('--tokenizer_name', type=str, default="mistralai/Mistral-7B-Instruct-v0.2", help='tokenizer to use for the model')
 
-    parser.add_argument('--checkpoint', type=str, default="models/fine-tune_run-1_MedInstruct-52k/checkpoint-4389/", help='checkpoint to load for model')
+    parser.add_argument('--checkpoint', type=str, default="models/Base-Model_Iterative-Training-Explanations_Iter-1/checkpoint-8000", help='checkpoint to load for model')
     parser.add_argument('--merge', dest='merge', action='store_true', help='boolean flag to set if model is merging')
     parser.add_argument('--no-merge', dest='merge', action='store_true', help='boolean flag to set if model is merging')
     parser.set_defaults(merge=False)
 
-    parser.add_argument('--exp_name', type=str, default="DPO Iteration-1 MedInstruct-52k + MEDLFQA", help='Describes the conducted experiment')
+    parser.add_argument('--exp_name', type=str, default="DPO Iteration-1 Base-Model", help='Describes the conducted experiment')
     parser.add_argument('--run', type=int, default=3, help='run number for wandb logging')
 
     # I/O paths for models, CT, queries and qrels
-    parser.add_argument('--save_dir', type=str, default="models/pre-train_run-3_MedLFQA/", help='path to model save dir')
+    parser.add_argument('--save_dir', type=str, default="models/Base-Model_DPO_Iter-1/", help='path to model save dir')
 
-    parser.add_argument("--train_file", default="", type=str)
-    parser.add_argument("--dev_file", default="", type=str)
+    parser.add_argument("--train_file", default="outputs/DPO_Preferences-Train_Base-Model_Med-Mix-1000_Iter-1_2024-07-11_18-12", type=str)
+    parser.add_argument("--dev_file", default="outputs/DPO_Preferences-Dev_Base-Model_Med-Mix-1000_Iter-1_2024-07-11_18-12", type=str)
 
     #Model Hyperparamenters
-    parser.add_argument("--max_length", type=int, default=5000)
-    parser.add_argument("--batch_size", default=16, type=int)
+    parser.add_argument("--max_length", type=int, default=3000)
+    parser.add_argument("--batch_size", default=8, type=int)
     parser.add_argument("--pooling", default="mean")
-    parser.add_argument("--train_epochs", default=15, type=int)
+    parser.add_argument("--train_epochs", default=5, type=int)
     parser.add_argument("--lr", type=float, default=2e-5)
 
     # Lora Hyperparameters
-    parser.add_argument("--lora_r", type=int, default=64)
+    parser.add_argument("--lora_r", type=int, default=32)
     parser.add_argument("--lora_dropout", type=float, default=0.1)
-    parser.add_argument("--lora_alpha", type=float, default=64)
+    parser.add_argument("--lora_alpha", type=float, default=32)
 
     #Speed and memory optimization parameters
     parser.add_argument("--fp16", action="store_true", help="Whether to use 16-bit (mixed) precision instead of 32-bit")
@@ -117,61 +117,58 @@ def main():
 
 
     train_dataset_load = json.load(open(f'{args.train_file}.json', encoding='utf8'))
-    train_dataset = {"id" : [], "text" : []}
+    train_dataset = {"prompt" : [], "chosen" : [], "rejected" : []}
 
-    for q_id in train_dataset_load:
-        train_dataset["id"].append(q_id)
-        train_dataset["text"].append(f'{train_dataset_load[q_id]["text"]}')
+    for i in range(len(train_dataset_load["prompt"])):
+        train_dataset["prompt"].append(f'{train_dataset_load["prompt"][i]}')
+        train_dataset["chosen"].append(f'{train_dataset_load["chosen"][i]}')
+        train_dataset["rejected"].append(f'{train_dataset_load["rejected"][i]}')
     train_dataset = Dataset.from_dict(train_dataset)
 
 
     eval_dataset_load = json.load(open(f'{args.dev_file}.json', encoding='utf8'))
-    eval_dataset = {"id" : [], "text" : []}
+    eval_dataset = {"prompt" : [], "chosen" : [], "rejected" : []}
 
-    for q_id in eval_dataset_load:
-        eval_dataset["id"].append(q_id)
-        eval_dataset["text"].append(f'{eval_dataset_load[q_id]["text"]}')
+    for i in range(len(eval_dataset_load["prompt"])):
+        eval_dataset["prompt"].append(f'{eval_dataset_load["prompt"][i]}')
+        eval_dataset["chosen"].append(f'{eval_dataset_load["chosen"][i]}')
+        eval_dataset["rejected"].append(f'{eval_dataset_load["rejected"][i]}')
     eval_dataset = Dataset.from_dict(eval_dataset)
 
     training_arguments = TrainingArguments(
         output_dir = args.save_dir,
         overwrite_output_dir=True,
-        evaluation_strategy="epoch",
-        save_strategy="epoch",
+        evaluation_strategy="steps",
+        save_strategy="steps",
+        save_steps= 1000,
+        eval_steps= 1000,
         save_total_limit= 5,
         num_train_epochs = args.train_epochs,
         per_device_train_batch_size= args.batch_size,
         optim = "paged_adamw_8bit",
-        logging_steps= 25,
+        logging_steps= 1000,
         learning_rate= args.lr,
         bf16= False,
         group_by_length= True,
         lr_scheduler_type= "constant",
-        #model load
         load_best_model_at_end= True,
-        
-        #Speed and memory optimization parameters
-        #gradient_accumulation_steps= args.gradient_accumulation_steps,
-        #gradient_checkpointing= args.gradient_checkpointing,
         fp16= args.fp16,
         report_to="wandb"
     )
     
     ## Data collator for completing with "Answer: YES" or "Answer: NO"
-    collator = DPODataCollatorWithPadding(tokenizer= tokenizer, padding="max_length", max_length= args.max_length, pooling= args.pooling)
+    collator = DataCollatorForCompletionOnlyLM("Answer: ", tokenizer= tokenizer)
 
     ## Setting sft parameters
-    trainer = SFTTrainer(
+    trainer = DPOTrainer(
         model= model,
+        ref_model= None,
         data_collator= collator,
         train_dataset= train_dataset,
         eval_dataset= eval_dataset,
         peft_config= peft_config,
-        max_seq_length= args.max_length,
-        dataset_text_field= "text",
         tokenizer= tokenizer,
         args= training_arguments,
-        packing= False,
     )
 
     ## Training
--- training/gen_iterative-DPO-preferences.py
+++ training/gen_iterative-DPO-preferences.py
@@ -2,28 +2,67 @@ import argparse
 import json
 import torch
 import os
-import datetime
+import re
+
+
 
 # Model Libs
 from transformers import AutoTokenizer, AutoModelForCausalLM
 from peft import PeftModel
+from tqdm import tqdm
+from datetime import datetime
 
 def safe_open_w(path: str):
     os.makedirs(os.path.dirname(path), exist_ok=True)
     return open(path, 'w', encoding='utf8')
 
-def calc_logits_next_token(model : object, tokenizer : object, text : str) -> str:   
+def calc_logits_next_token(model : object, tokenizer : object, text : str, token_lists : dict) -> str:   
     tokenized = tokenizer(text, return_tensors="pt")
     tokenized["input_ids"] = tokenized.input_ids.to(device="cuda")
     tokenized["attention_mask"] = tokenized.attention_mask.to(device="cuda")
 
     # We could use do_sample=False and disable top_k and top_p to get a deterministic output
-    outputs = model.generate(**tokenized, max_new_tokens = 5, do_sample = False, temperature = 1.0, pad_token_id = tokenizer.eos_token_id)
-    
-    #TO:DO: Output Generation Logits here
-    
+    outputs = model.generate(**tokenized, max_new_tokens = 5, do_sample = False, temperature = 1.0, pad_token_id = tokenizer.eos_token_id, return_dict_in_generate=True, output_scores=True)
 
-    return None
+    greedy_score = outputs.scores[0]
+
+    no_scores = [greedy_score[0][token[-1]] for token in token_lists["no_tokens"]]
+    yes_scores = [greedy_score[0][token[-1]] for token in token_lists["yes_tokens"]]
+
+    max_scores = torch.softmax(torch.tensor([max(no_scores), max(yes_scores)]), dim=0).tolist()
+    avg_scores = torch.softmax(torch.tensor([sum(no_scores)/3.0, sum(yes_scores)/3.0]), dim=0).tolist()
+
+    return {"max_scores" : max_scores, "avg_scores" : avg_scores}
+
+def order_DPO_preferences(res):
+    for q_id_res in res:
+        res[q_id_res]["ids"] = [id for _, id in sorted(zip(res[q_id_res]["explanations_scores"], res[q_id_res]["ids"]), key=lambda x: x[0][1], reverse=True)]
+        res[q_id_res]["explanations_scores"] = sorted(res[q_id_res]["explanations_scores"], key=lambda x: x[1], reverse=True)
+
+    prompt = json.load(open("prompts/DPOPrompts.json"))["gen_explanations"]
+
+    dpo_preferences_train = {"prompt" : [], "chosen" : [], "rejected" : []}
+    dpo_preferences_dev = {"prompt" : [], "chosen" : [], "rejected" : []}
+
+    n_train = 0
+
+    for q_id_res in res:
+        if len(res[q_id_res]["explanations_scores"]) >= 7:
+
+            for i in range(3):
+                dpo_preferences_train["prompt"].append(prompt)
+                dpo_preferences_train["chosen"].append(res[q_id_res]["explanations_scores"][i][0])
+                dpo_preferences_train["rejected"].append(res[q_id_res]["explanations_scores"][-(i+1)][0])
+                n_train += 1
+
+            # 1/9 of the data is used for dev
+            if n_train % 9 == 0: 
+                dpo_preferences_dev["prompt"].append(prompt)
+                dpo_preferences_dev["chosen"].append(res[q_id_res]["explanations_scores"][3][0])
+                dpo_preferences_dev["rejected"].append(res[q_id_res]["explanations_scores"][-4][0])
+    
+    return dpo_preferences_train, dpo_preferences_dev
+    
 
 def gen_DPO_preferences():
     parser = argparse.ArgumentParser()
@@ -66,24 +105,38 @@ def gen_DPO_preferences():
 
     res = {}
 
-    for q_id in queries:
+    token_lists = {"no_tokens" : [tokenizer.encode("N"), tokenizer.encode("No"), tokenizer.encode("no")], "yes_tokens" : [tokenizer.encode("Y"), tokenizer.encode("Yes"), tokenizer.encode("yes")]}
+
+    for q_id in tqdm(queries):
         q_id_res = q_id.split("_")[0]
-        if q_id_res not in res:
-            res[q_id_res] = {"explanations_scores" : [], "ids" : []}
 
-        res[q_id_res]["ids"].append(q_id)
-        text = f'{queries[q_id]["text"].split("Answer:")[0]} Answer: '
+        text = f'{queries[q_id]["text"].split("Answer:")[0]} Answer:'
+        explanation = re.search(".*The following is an explanation of the statement's validity, by another expert using on the information available within the CTRs.\\n\\n(.*)\\n\\nBased on your analysis of the CTRs,.*", text)
 
-        logits_next_token = calc_logits_next_token(model, tokenizer, text)
-        res[q_id_res]["explanations_scores"].append((text, logits_next_token))
+        if explanation != None:
 
-        with safe_open_w(f'{args.output_dir}DPO_Explanation-Preferences_{args.exp_name}_{datetime.now().strftime("%Y-%m-%d_%H-%M")}.json') as output_file:
-            output_file.write(json.dumps(res, ensure_ascii=False, indent=4))
+            if q_id_res not in res:
+                res[q_id_res] = {"explanations_scores" : [], "ids" : [], "gold_label" : 0 if "No" in queries[q_id]["text"].split("Answer:")[1] else 1}
 
+            res[q_id_res]["ids"].append(q_id)
+            logits_next_token = calc_logits_next_token(model, tokenizer, text, token_lists)
+
+            if explanation != None:
+                res[q_id_res]["explanations_scores"].append((explanation.group(1), logits_next_token["max_scores"][res[q_id_res]["gold_label"]]))
+
+    res_train, res_dev = order_DPO_preferences(res)
+
+    with safe_open_w(f'{args.output_dir}DPO_Explanation-Scores_{args.exp_name}_{datetime.now().strftime("%Y-%m-%d_%H-%M")}.json') as output_file:
+        output_file.write(json.dumps(res, ensure_ascii=False, indent=4))
+
+    with safe_open_w(f'{args.output_dir}DPO_Preferences-Train_{args.exp_name}_{datetime.now().strftime("%Y-%m-%d_%H-%M")}.json') as output_file:
+        output_file.write(json.dumps(res_train, ensure_ascii=False, indent=4))
+
+    with safe_open_w(f'{args.output_dir}DPO_Preferences-Dev_{args.exp_name}_{datetime.now().strftime("%Y-%m-%d_%H-%M")}.json') as output_file:
+        output_file.write(json.dumps(res_dev, ensure_ascii=False, indent=4))
 
 def main():
     gen_DPO_preferences()
-    # parse_DPO_preferences()
 
 if __name__ == '__main__':
     main()
